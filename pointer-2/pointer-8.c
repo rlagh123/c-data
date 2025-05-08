b2e3503c5f67d0473a9e12195c2d8f5bf13838fa
호 김 <u7sgsiishis@gmail.com>
2025-05-08 15:34:43 +0900

    포인터증감

---
+++ pointer-2/pointer-8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    int* pi = &i;
+    // i=>10, pi는 i주소
+    printf("i = %d, pi = %p\n", i, pi);
+
+    (*pi)++;  // pi 값을 가지고 오고
+    // 다음문장으로 갈때 값을 1증가
+    printf("i = %d, pi = %p\n", i, pi);//11
+
+    *pi++; //pi 값을 가져오고 pi 주소를 증가, 
+    printf("i = %d, pi = %p\n", i, pi);//11
+    return 0;
+}
